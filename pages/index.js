1eb2e471e3b85fc31e88b3e8820e5dc904cef0e8
Paulo Henrique <pauloh1288@gmail.com>
2019-09-05 22:17:11 -0300

    base config next.js + tailwindcss

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import '../assets/style.css';
+
+export default function Index() {
+  return (
+    <div className="p-4 bg-purple-300">
+      <p>Hello Next.js with tailwindcss</p>
+    </div>
+  );
+}
