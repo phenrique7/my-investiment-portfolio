02e8b66f7129a19f83048d41b73c43464fae79a3
Paulo Henrique <pauloh1288@gmail.com>
2019-10-27 20:13:21 -0300

    muda parâmetros de identificação de perfil

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,8 +1,8 @@
 export const LS_USER_DATA_KEY = '__mci_user_data__';
 export const MAX_QUESTIONS = 6;
 export const FIRST_QUESTION = 0;
-export const CONSERVATIVE_PROFILE_LIMIT = 8;
-export const AGRESSIVE_PROFILE_LIMIT = 16;
+export const CONSERVATIVE_PROFILE_LIMIT = 6;
+export const AGRESSIVE_PROFILE_LIMIT = 13;
 
 export const CONSERVATIVE_PROFILE_DATA = {
   colors: [
